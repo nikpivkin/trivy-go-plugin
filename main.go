3dc21ac6ff3f24fc602a160645b7ee6ea3cdb341
malikovmm <91451961+malikovmm@users.noreply.github.com>
2023-04-12 17:29:49 +0600

    Build: initialize base trivy go plugin (#2)
    * build: remove deprecated flags, add other os support
    
    * feat: add help message and read json functionality
    
    * refactor: move help flag check to the main, change getFlagValue return type
    
    * refactor: got rid of else
    
    * refactor: reduce the length of getFlagValue

--- main.go
+++ main.go
@@ -1,32 +1,93 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+
+	"golang.org/x/exp/slices"
+
+	k8sReport "github.com/aquasecurity/trivy/pkg/k8s/report"
+	"github.com/aquasecurity/trivy/pkg/types"
 )
 
 var (
-	tempFileName = "trivy-go-plugin-temp.json"
+	tempJsonFileName = "trivy-go-plugin-temp.json"
+	version          = "dev"
 )
 
 func main() {
-	trivyCommand := os.Args[1 : len(os.Args)-1]
-	tempFilePath := filepath.Join(os.TempDir(), tempFileName)
-	defer removeFile(tempFilePath)
-
-	cmdArgs := append(trivyCommand, "--format", "json", "--output", tempFilePath)
-	if err := exec.Command("trivy", cmdArgs...).Run(); err != nil {
-		log.Fatalf("failed to build report: %v", err)
+	if slices.Contains(os.Args, "-h") || slices.Contains(os.Args, "--help") {
+		helpMessage()
+	}
+	pluginOutput := getFlagValue("--plugin-output")
+	if pluginOutput == "" {
+		log.Fatal("flag --plugin-output is required")
+	}
+	trivyOutputFileName := getFlagValue("--output")
+	if trivyOutputFileName == "" {
+		trivyOutputFileName = filepath.Join(os.TempDir(), tempJsonFileName)
+		defer removeFile(trivyOutputFileName)
 	}
 
+	if err := makeTrivyJsonReport(trivyOutputFileName); err != nil {
+		log.Fatalf("failed to make trivy report: %v", err)
+	}
+	_, err := getReportFromJson(trivyOutputFileName)
+	if err != nil {
+		log.Fatalf("failed to get report from json: %v", err)
+	}
+
+	if err := saveResult(pluginOutput, []byte{}); err != nil {
+		log.Fatalf("failed to save result: %v", err)
+	}
 }
-func getScanType() string {
-	return os.Args[1]
+
+func getReportFromJson(jsonFileName string) (*types.Report, error) {
+	if !isK8s() {
+		return readJson[types.Report](jsonFileName)
+	}
+
+	k8sParsedReport, err := readJson[k8sReport.Report](jsonFileName)
+	if err != nil {
+		return nil, err
+	}
+
+	var resultsArr types.Results
+	for _, vuln := range k8sParsedReport.Vulnerabilities {
+		resultsArr = append(resultsArr, vuln.Results...)
+	}
+	for _, misc := range k8sParsedReport.Misconfigurations {
+		resultsArr = append(resultsArr, misc.Results...)
+	}
+	return &types.Report{
+		Results: resultsArr,
+	}, nil
 }
-func getOutputFileName() string {
-	return os.Args[len(os.Args)-1]
+
+func readJson[T any](jsonFileName string) (*T, error) {
+	jsonFile, err := os.Open(jsonFileName)
+	if err != nil {
+		return nil, err
+	}
+
+	defer closeFile(jsonFile)
+
+	var out T
+	if err := json.NewDecoder(jsonFile).Decode(&out); err != nil {
+		return nil, err
+	}
+	return &out, nil
+}
+
+func isK8s() bool {
+	if slices.Contains(os.Args, "kubernetes") || slices.Contains(os.Args, "k8s") {
+		return true
+	}
+	return false
 }
 
 func removeFile(file string) {
@@ -34,8 +95,59 @@ func removeFile(file string) {
 		log.Fatalf("failed to remove file %v", err)
 	}
 }
+
 func closeFile(file *os.File) {
 	if err := file.Close(); err != nil {
 		log.Fatalf("failed to remove file %v", err)
 	}
+}
+
+func getFlagValue(flag string) string {
+	flagIndex := slices.Index(os.Args, flag)
+	if flagIndex != -1 && (len(os.Args)-1) > flagIndex { // the flag exists and it is not the last argument
+		return os.Args[flagIndex+1]
+	}
+	return ""
+}
+
+func saveResult(filename string, result []byte) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	_, err = file.Write(result)
+	if err != nil {
+		return err
+	}
+	defer closeFile(file)
+	return nil
+}
+
+func makeTrivyJsonReport(outputFileName string) error {
+	trivyCommand := os.Args[1 : len(os.Args)-2]
+	cmdArgs := append(trivyCommand, "--format", "json", "--output", outputFileName)
+	cmd := exec.Command("trivy", cmdArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func helpMessage() {
+	_, err := fmt.Printf(`
+trivy-go-plugin v%s
+Usage: trivy trivy-go-plugin [-h,--help] command target filename
+ A Trivy common plugin.
+Options:
+  -h, --help    Show usage.
+Examples:
+  # example
+  trivy trivy-go-plugin
+`, version)
+	if err != nil {
+		log.Fatalf("Failed to display help message %v", err)
+	}
+	os.Exit(0)
 }
